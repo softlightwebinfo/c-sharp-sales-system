9205ac5e0703a01012f625223f4c97bc98eb8fe5
Rafael González Muñoz <rafael.gonzalez.1737@gmail.com>
2020-10-03 21:51:28 +0200

    Refactor de código en clases y empezar a implementar patron de diseño SOLID

---
+++ Sales system/Migrations/20201003194639_InitialCreate.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Sales_system.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clients",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    client_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clients", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    product_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    unit_price = table.Column<decimal>(type: "numeric(16,2)", precision: 16, scale: 2, nullable: false),
+                    amount = table.Column<decimal>(type: "numeric(16)", precision: 16, nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    user_password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    username = table.Column<string>(type: "character varying(49)", maxLength: 49, nullable: false),
+                    first_name = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    surnames = table.Column<string>(type: "character varying", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sales",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    total = table.Column<decimal>(type: "numeric(16,2)", precision: 16, scale: 2, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "now()"),
+                    fk_client_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sales", x => x.id);
+                    table.ForeignKey(
+                        name: "sales_clients_id_fk",
+                        column: x => x.fk_client_id,
+                        principalTable: "clients",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "concepts",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    fk_sale_id = table.Column<long>(type: "bigint", nullable: false),
+                    price_unit = table.Column<decimal>(type: "numeric(16,2)", precision: 16, scale: 2, nullable: false),
+                    amount = table.Column<decimal>(type: "numeric(16,2)", precision: 16, scale: 2, nullable: true, defaultValueSql: "0.0"),
+                    fk_product_id = table.Column<int>(type: "integer", nullable: false),
+                    total = table.Column<decimal>(type: "numeric(16,2)", precision: 16, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_concepts", x => x.id);
+                    table.ForeignKey(
+                        name: "concepts_products_id_fk",
+                        column: x => x.fk_product_id,
+                        principalTable: "products",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "concepts_sales_id_fk",
+                        column: x => x.fk_sale_id,
+                        principalTable: "sales",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_concepts_fk_product_id",
+                table: "concepts",
+                column: "fk_product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_concepts_fk_sale_id",
+                table: "concepts",
+                column: "fk_sale_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_sales_fk_client_id",
+                table: "sales",
+                column: "fk_client_id");
+
+            migrationBuilder.CreateIndex(
+                name: "table_name_id_index",
+                table: "sales",
+                column: "id");
+
+            migrationBuilder.CreateIndex(
+                name: "users_username_uindex",
+                table: "users",
+                column: "username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "concepts");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "sales");
+
+            migrationBuilder.DropTable(
+                name: "clients");
+        }
+    }
+}
